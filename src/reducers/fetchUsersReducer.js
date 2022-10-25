7cbad68ff178758a66dc887ea57a9b6b720e8a86
GOLDEN BUDDHA <sbibek086@gmail.com>
2022-10-25 19:28:19 +0545

    Update fetchUsersReducer.js

--- src/reducers/fetchUsersReducer.js
+++ src/reducers/fetchUsersReducer.js
@@ -24,9 +24,14 @@ export default function(state = initState, action) {
         return {
             ...state,
             userList:action.payload
-        }
-
-        
+            /* about ...state -imagine that you have a component that accepts N number of properties. Passing these down can be tedious if the number grows.
+                   <Component x={} y={} z={} />
+                Thus instead you do this, wrap them up in an object and use the spread notation
+                     var props = { x: 1, y: 1, z:1 };
+                   <Component {...props} />
+            which will unpack it into the props on your component, ie you "never" use {... props} inside your render() function but only when you pass props down to 
+           another component. Use your unpacked props as normal this.props.x  */
+        }    
     }
     return state;
 }
